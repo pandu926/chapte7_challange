e6713f824cbbb6a4b08647a1f3153db008f9ad39
pandu926 <63030609+pandu926@users.noreply.github.com>
2022-09-05 13:35:38 +0700

    try instal ssl

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 80;
 const userRoute = require("./src/user/user.route");
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./src/config/swagger');
 const postRoute = require("./src/post/post.route");
+const fs = require("fs");
+const https = require('https')
 const cors = require("cors");
 
+// ssl 
+const options = {
+    key: fs.readFileSync("./ssl/private.key"),
+    cert: fs.readFileSync('./ssl/certificate.crt'),
+};
 
 app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -16,10 +23,12 @@ app.use(express.urlencoded());
 app.use(userRoute);
 app.use(postRoute);
 
+
+
 app.get("/", (req, res) => {
     res.send("berhasil");
 })
 
-app.listen(port, () => {
-    console.log("sukses");
-})
+https.createServer(options, app).listen(port, function() {
+    console.log("Express server listening on port " + port);
+});
